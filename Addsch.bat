6d67d9e292d4ec7600ef617cb772e45bbf9bb4d6
maytom2016 <qwerty448qwe@163.com>
2021-07-31 22:09:06 +0800

    Update Addsch.bat
    该文件向windows任务计划中添加一个任务，定期1个小时更换目录中的壁纸，配合其他两个脚本使用。

--- Addsch.bat
+++ Addsch.bat
@@ -1,7 +1,7 @@
 @echo off
-set "path1=%CD%\HideAcWp.vbs"  
+set "path1=%CD%\HideAcWp-task.vbs"  
 echo %path1%                                 rem 换壁纸的路径
-set "hour=1"                                 rem 每小时换一次
+set "hour=1"                                    rem 每小时换一次
 schtasks.exe /create /tn "AutoChangeWallpapertest" /tr %path1% /sc HOURLY /mo 1 /f
 echo 创建成功，请检查...
 pause
